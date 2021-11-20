70948c4f5bb8ecc1294dca5d49b59f256d6ed8d4
Администратор <@DESKTOP-PJPICF5>
2021-11-21 00:27:59 +0800

    Добавьте файлы проекта.

---
+++ 4_2_Task/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+int m = 0;
+do
+{
+    if (n!=0)
+    {
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        break;
+    }
+    if (n>0)
+    {
+        k++;
+    }
+   if (n<0)
+    {
+        m++;
+    }
+}
+while (n != 0);
+if (k++>m++)
+{
+    Console.WriteLine("Количество положительных чисел больше, чем отрицательных и равно {0}", +k);
+}
+else
+{
+    Console.WriteLine("Количество отрицательных чисел больше, чем положительных и равно {0}", +m-1);
+}
+    
+
+
---
+++ 4_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1, a = 1; (i <= 20) && (a <= 400); i++, a++)
+{
+    a =( 2 * i - 1)+a-1;
+    if (i == n + 1)
+    {
+        break;
+    }
+    Console.WriteLine("Квадрат числа {0} равен {1}", i, a);
+}
